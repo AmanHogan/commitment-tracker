51e303dfce7016b8bd0f46a7b94d1b4ab1daf3ea
Aman Hogan <ahoganbailey@gmail.com>
2026-04-06 15:37:33 -0500

    Added events and subevents

---
+++ backend/src/main/java/com/amanhogan/commitment_tracker/model/EventSubItem.java
@@ -0,0 +1,49 @@
+package com.amanhogan.commitment_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "event_sub_items")
+@EntityListeners(AuditingEntityListener.class)
+public class EventSubItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private DevelopmentCommitmentTwo event;
+
+    @Column(nullable = false)
+    private String subEventName;
+
+    private String description;
+
+    private LocalDate started;
+
+    private LocalDate finished;
+
+    private Boolean done;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
